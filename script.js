52736db2f9bbfac927f9d3bdf6dc294d4fc0d561
Yakkobi <289114684+Yakkobi@users.noreply.github.com>
2026-07-16 18:12:04 -0400

    Add comments to code
    Added comments to make the code easier to
    understand.

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let currentInput = "";
 let previousValue = null;
 let operator = null;
 
+/* The "calculator" function, also treated as an object because functions in JS can do that.. */
 function calculator() {
   return {
     1: 1,
@@ -29,15 +30,20 @@ function calculator() {
   };
 }
 
+/* Creates a listener that passes the HTML value to the "rawPressedValue" variable */
 btn.forEach((buttonValue) => {
   buttonValue.addEventListener("click", function (buttonPressed) {
     const rawPressedValue =
       buttonPressed.target.dataset.value || buttonPressed.target.dataset.action;
 
+    /* Returns the value from each button press to the "mappedValue" variable */
     const mappedValue = returnCalcValue[rawPressedValue];
 
+    /* Adds the "firstValue" property to the calculator function */
     calculator.firstValue = mappedValue;
 
+    /* If the calculator.firstValue property in NOT undefined, it changes
+the screen to match the firstValue property via the textContent DOM property */
     if (calculator.firstValue !== undefined) {
       screen.textContent = calculator.firstValue;
     }
@@ -46,6 +52,7 @@ btn.forEach((buttonValue) => {
   });
 });
 
+/* The "operate" function used for the actual calculation method */
 function operate(operator, a, b) {
   if (operator === "add") {
     return a + b;
