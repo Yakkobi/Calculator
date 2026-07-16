9f75f6d465ba246139673ebaff2c7a87885632d7
Yakkobi <289114684+Yakkobi@users.noreply.github.com>
2026-07-15 20:29:44 -0400

    Change button click function to change variable
    Needed to create a variable outside the function
    that can be changed by the button presses.

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const buttons = document.querySelectorAll(".btn");
+let selectedButton = null;
 
+/* returns the value for the button pressed (the values are in the HTML file) */
 buttons.forEach((button) => {
-  button.addEventListener("click", (i) => {
-    console.log(i.target.dataset.value);
+  button.addEventListener("click", function (i) {
+    selectedButton = i.target.dataset.value;
+    console.log(selectedButton);
   });
 });
