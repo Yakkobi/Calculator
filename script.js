c8e2385ae00c1afdb03b06cedbe1d682ae5e219a
Yakkobi <289114684+Yakkobi@users.noreply.github.com>
2026-07-15 20:39:30 -0400

    Change the variable to the "screen" object
    The button presses need to change multiple
    values within an object, and then another
    function can operate on the values stored
    within the object.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const buttons = document.querySelectorAll(".btn");
-let selectedButton = null;
+const screen = {
+  firstValue: null,
+  secondValue: null,
+  thirdValue: null,
+};
 
 /* returns the value for the button pressed (the values are in the HTML file) */
 buttons.forEach((button) => {
   button.addEventListener("click", function (i) {
-    selectedButton = i.target.dataset.value;
-    console.log(selectedButton);
+    screen.firstValue = i.target.dataset.value;
+    console.log(screen.firstValue);
   });
 });
